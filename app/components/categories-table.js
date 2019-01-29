3957ea0de857540f8d730a59182b08e363a1a312
ikuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-01-29 15:58:17 +0100

    Added details/update/delete modals (update form conflicting with modal)

--- app/components/categories-table.js
+++ app/components/categories-table.js
@@ -1,4 +1,30 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    selectedCategory: null,
+    isDetailsModalOpen: false,
+    isUpdateModalOpen: false,
+    isDeleteModalOpen: false,
+
+    actions: {
+        openModal(modal, category) {
+            this.set(`is${modal}ModalOpen`, true);
+            this.set('selectedCategory', category);
+        },
+        closeModal(modal) {
+            this.set(`is${modal}ModalOpen`, false);
+        },
+        closedModal() {
+            this.set('selectedCategory', null);
+        },
+        submitCategory() {
+            this.set('isDetailsModalOpen', false);
+        },
+        updateCategory() {
+            this.set('isUpdateModalOpen', false);
+        },
+        deleteCategory() {
+            this.set('isDeleteModalOpen', false);
+        }
+    }
 });
