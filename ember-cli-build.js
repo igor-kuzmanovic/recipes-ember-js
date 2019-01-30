146340b27818f7585bcc6889342fed4c437afdfc
ikuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-01-30 12:16:45 +0100

    Added the ingredients/category/tags options for recipe creating/updating

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,6 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'ember-bootstrap': {
       'bootstrapVersion': 4,
-      'importBootstrapFont': false,
       'importBootstrapCSS': false
     }
   });
