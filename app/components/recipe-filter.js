adefe08fe9a2444d5932c1fdfd29dd8c249898e7
ikuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-02-04 16:33:32 +0100

    Filter refactor

--- app/components/recipe-filter.js
+++ app/components/recipe-filter.js
@@ -51,19 +51,62 @@ export default Component.extend({
         onDateChange(date) {
             this.set('date', date);
 
-            this.get('onFilterResults')(this.get('filter'));
+            this.send('onFilterChange');
         },
 
         onCategoryChange(category) {
             this.set('category', category);
 
-            this.get('onFilterResults')(this.get('filter'));
+            this.send('onFilterChange');
         },
 
         onTagChange(tag) {
             this.set('tag', tag);
 
-            this.get('onFilterResults')(this.get('filter'));
+            this.send('onFilterChange');
+        },
+
+        onFilterChange() {
+            let filter = {};
+
+            let date = this.get('date');
+            if (date) {
+                if (date) {
+                    filter.date = moment(date).format('YYYY-MM-DD');
+                }
+            }
+
+            let categories = this.get('category');
+            if (categories) {
+                if (categories && categories.length > 0) {
+                    filter.category = '';
+
+                    categories.forEach(function(category, key, array) {
+                        filter.category  += category.id;
+
+                        if (!Object.is(array.length - 1, key)) {
+                            filter.category  += ',';
+                        }
+                    });
+                }
+            }
+
+            let tags = this.get('tag');
+            if (tags) {
+                if (tags && tags.length > 0) {
+                    filter.tag = '';
+
+                    tags.forEach(function(tag, key, array) {
+                        filter.tag += tag.id;
+
+                        if (!Object.is(array.length - 1, key)) {
+                            filter.tag += ',';
+                        }
+                    });
+                }
+            }
+
+            this.get('onFilterResults')(filter);
         },
 
         onClear() {
